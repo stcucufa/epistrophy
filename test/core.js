64258dc2cd1f8f242f85d8ec2d6a3f35abf2c76a
stcucufa <104885483+stcucufa@users.noreply.github.com>
2025-07-29 10:57:17 +0200

    4O02 Core: ramps with infinite duration (#119)
    Handle ramps with (effective) inifinite duration, which never end and
    get repeatedly called with p=0.

--- test/core.js
+++ test/core.js
@@ -573,3 +573,50 @@ test("Multiple errors and recovery (async)", async t => {
             sync((fiber, scheduler) => { scheduler.setFiberRate(fiber, -1); })
         ));
 });
+
+// 4O02 Core: ramps with infinite duration
+
+test("Ramp with infinite duration", t => {
+    const ps = [[0, 0], [0, 888], [0, 8888], [0, 88888]];
+    const scheduler = run(new Fiber().
+        ramp(Infinity, (p, fiber) => {
+            t.equal([p, fiber.now], ps.shift(), `ramp did progress (${p})`);
+        }),
+        888
+    );
+    scheduler.clock.now = 8888;
+    scheduler.clock.now = 88888;
+    t.equal(ps, [], "ramp went through all updates");
+});
+
+test("Ramp with infinite duration (rate changes)", t => {
+    const ps = [[0, 0, 0], [0, 222, 222], [0, 333, 444], [0, 555, 888], [0, 0, 1443]];
+    const fiber = new Fiber().
+        ramp(Infinity, (p, fiber, scheduler) => {
+            t.equal([p, fiber.now, scheduler.now], ps.shift(), `ramp did progress (${p}/${fiber.now})`);
+        });
+    const scheduler = run(fiber, 222);
+    scheduler.setFiberRate(fiber, 0.5);
+    scheduler.clock.now = 444;
+    scheduler.setFiberRate(fiber, 0);
+    scheduler.clock.now = 777;
+    scheduler.setFiberRate(fiber, 2);
+    scheduler.clock.now = 888;
+    scheduler.setFiberRate(fiber, -1);
+    scheduler.clock.now = Infinity;
+    t.equal(ps, [], "ramp went through all updates");
+});
+
+test("Ramp with infinite duration (rate changes back and forth)", t => {
+    const ps = [[0, 0, 0], [0, 444, 444], [0, 111, 777], [0, 333, 999]];
+    const fiber = new Fiber().
+        ramp(Infinity, (p, fiber, scheduler) => {
+            t.equal([p, fiber.now, scheduler.now], ps.shift(), `ramp did progress (${p}/${fiber.now})`);
+        });
+    const scheduler = run(fiber, 444);
+    scheduler.setFiberRate(fiber, -1);
+    scheduler.clock.now = 777;
+    scheduler.setFiberRate(fiber, 1);
+    scheduler.clock.now = 999;
+    t.equal(ps, [], "ramp went through all updates");
+});
