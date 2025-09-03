dcb75b32f261b45cc0857aaf9ce2b9c4c9ba68be
stcucufa <githubnyou@romulusetrem.us>
2025-09-03 23:41:29 +0200

    4V01 Cars example update

--- examples/cars/cars.js
+++ examples/cars/cars.js
@@ -1,76 +1,75 @@
-import Scheduler from "../../lib/scheduler.js";
-import { Last, First, Gate } from "../../lib/fiber.js";
+import { run, First } from "../../lib/shell.js";
 
 const Width = 800;
 const Height = 600;
 const Danger = [-100, 250];
 const GameDuration = 5000;
 const N = [7, 12];
-const Images = ["red1.png", "red2.png", "gray1.png", "gray2.png", "crash1.png", "crash2.png", "flag1.png", "flag2.png"];
+const Srcs = ["red1.png", "red2.png", "gray1.png", "gray2.png", "crash1.png", "crash2.png", "flag1.png", "flag2.png"];
 
-const loadImage = async (src) => new Promise((resolve, reject) => {
-    const image = new Image();
-    image.onload = () => { resolve([src, image]); };
-    image.onerror = () => reject(`Could not load image ${src}`);
-    image.src = src;
-    if (image.complete) {
-        resolve([src, image]);
-    }
-});
+run().
 
-const fiber = Scheduler.run().
+    // Set up
+    sync(({ scope }) => {
+        Object.assign(scope, {
+            canvas: document.querySelector("canvas"),
+            progress: document.querySelector("progress"),
+            lanes: [50, 200, 350],
+            cars: [{ images: ["red1.png", "red2.png"], frame: 0, x: 20, lane: 1, v: 0 }],
+            images: {}
+        });
+        scope.progress.value = 0;
+        scope.progress.max = GameDuration;
+        scope.canvas.width = Width;
+        scope.canvas.height = Height;
+    }).
 
-    // Create the game object
-
-    exec(() => ({
-        canvas: document.querySelector("canvas"),
-        progress: document.querySelector("progress"),
-        lanes: [50, 200, 350],
-        cars: [{ images: ["red1.png", "red2.png"], frame: 0, x: 20, lane: 1, v: 0 }],
-        images: {},
-    })).
-
-    // Load all resources before continuing
-
-    spawn(fiber => {
-        for (const image of Images) {
-            fiber.spawn(fiber => fiber.exec(async () => loadImage(image)));
-        }
-        fiber.
-            join(Last).
-            exec(({ parent: { value: game }, value: pairs }) => {
-                for (const [src, image] of pairs) {
-                    game.images[src] = image;
+    // Load all images and collect them in an object using their relative src
+    // as key.
+    macro(fiber => {
+        for (const src of Srcs) {
+            fiber.spawn(fiber => fiber.async(async () => new Promise((resolve, reject) => {
+                const image = new Image();
+                image.onload = () => { resolve(image); };
+                image.onerror = () => reject(`Could not load image ${src}`);
+                image.dataset.key = src;
+                image.src = src;
+                if (image.complete) {
+                    resolve(image);
                 }
-                return game;
-            })
+            }), {
+                asyncWillEndWithValue(image, { scope }) {
+                    scope.image = image;
+                }
+            }
+            ));
+        }
+        fiber.join({
+            childFiberDidJoin(child) {
+                const image = child.scope.image;
+                child.parent.scope.images[image.dataset.key] = image;
+            }
+        });
     }).
     join().
 
-    // Press a key to begin
+    // Show the splash screen and way for a key press to begin.
+    sync(({ scope }) => {
+        const context = scope.canvas.getContext("2d");
+        context.save();
+        context.fillStyle = "#1d2b53";
+        context.font = "italic 96px system-ui, sans-serif";
+        context.textAlign = "center";
+        context.textBaseline = "middle";
+        context.fillText("RACE!", Width / 2, Height / 2);
+        context.restore();
+    }).
+    event(window, "keydown", {
+        eventWasHandled(event) {
+            event.preventDefault();
+        }
+    }).
 
-    spawn(fiber => fiber.
-        effect(({ value: game }) => {
-            game.progress.value = 0;
-            game.progress.max = GameDuration;
-            game.canvas.width = Width;
-            game.canvas.height = Height;
-            const context = game.canvas.getContext("2d");
-            context.save();
-            context.fillStyle = "#1d2b53";
-            context.font = "96px system-ui, sans-serif";
-            context.textAlign = "center";
-            context.textBaseline = "middle";
-            context.fillText("RACE!", Width / 2, Height / 2);
-            context.restore();
-        }).
-        event(window, "keydown")
-    ).
-    join().
-
-    // Game loop
-    // FIXME 4H0H Restart cars game
-    
     spawn(fiber => fiber.
 
         // Setup other cars: introduce a car after its delay, and setup its
@@ -80,36 +79,37 @@ const fiber = Scheduler.run().
             const n = Math.floor(Math.random() * (N[1] - N[0])) + N[0]
             for (let i = 0; i < n; ++i) {
                 fiber.spawn(fiber => fiber.
-                    delay(Math.random() * GameDuration).
-                    exec(({ value: game }) => {
-                        const car = {
+                    ramp(Math.random() * GameDuration).
+                    sync(({ scope }) => {
+                        scope.car = {
                             images: ["gray1.png", "gray2.png"],
-                            lane: Math.floor(Math.random() * game.lanes.length),
+                            lane: Math.floor(Math.random() * scope.lanes.length),
                             frame: 0,
                             x: Width,
                             v: -50
                         };
-                        game.cars.push(car);
-                        return car;
                     }).
                     repeat(fiber => fiber.
-                        delay(50).
-                        effect(({ parent, value: car }) => {
-                            car.x += car.v;
-                        }), {
-                            repeatShouldEnd: (_, { parent, value: car }) => car.x > Danger[0] &&
-                                car.x < Danger[1] && car.lane === parent.value.cars[0].lane
+                        ramp(50).
+                        sync(({ scope: { car } }) => { car.x += car.v; }),
+                        {
+                            repeatShouldEnd: (_, { scope: { car, cars } }) => car.x > Danger[0] && car.x < Danger[1] &&
+                                car.lane === cars[0].lane
                         }
                     )
                 );
             }
             fiber.
-                join(Gate).
-                effect(({ value: game }) => {
+                join({
+                    childFiberDidJoin(child) {
+                        child.parent.scope.cars.push(child.scope.car);
+                    }
+                }).
+                sync(({ scope }) => {
                     // If a car fiber ended, then there was a crash.
-                    game.cars.length = 1;
-                    game.cars[0].images = ["crash1.png", "crash2.png"];
-                    game.cars[0].x = 200;
+                    scope.cars.length = 1;
+                    scope.cars[0].images = ["crash1.png", "crash2.png"];
+                    scope.cars[0].x = 200;
                 })
         }).
 
@@ -119,12 +119,12 @@ const fiber = Scheduler.run().
             spawn(fiber => fiber.
                 repeat(fiber => fiber.
                     event(window, "keydown", {
-                        eventWasHandled(event, { value: game }) {
+                        eventWasHandled(event, { scope }) {
                             if (event.key === "ArrowUp") {
-                                game.cars[0].lane = Math.max(0, game.cars[0].lane - 1);
+                                scope.cars[0].lane = Math.max(0, scope.cars[0].lane - 1);
                                 event.preventDefault();
                             } else if (event.key === "ArrowDown") {
-                                game.cars[0].lane = Math.min(game.lanes.length - 1, game.cars[0].lane + 1);
+                                scope.cars[0].lane = Math.min(scope.lanes.length - 1, scope.cars[0].lane + 1);
                                 event.preventDefault();
                             }
                         }
@@ -132,30 +132,28 @@ const fiber = Scheduler.run().
                 )
             ).
             spawn(fiber => fiber.
-                delay(GameDuration).
-                effect(({ value: game }) => {
-                    game.cars.length = 1;
-                    game.cars[0].images = ["flag1.png", "flag2.png"];
-                    game.cars[0].x = 200;
-                    game.cars[0].lane = 1;
+                ramp(GameDuration).
+                sync(({ scope }) => {
+                    scope.cars.length = 1;
+                    scope.cars[0].images = ["flag1.png", "flag2.png"];
+                    scope.cars[0].x = 200;
+                    scope.cars[0].lane = 1;
                 })
             ).
             join(First)
         ).
 
-        join(Gate)
+        join()
     ).
 
     // Draw loop
     spawn(fiber => fiber.
-        ramp(Infinity, {
-            rampDidProgress(_, { value: game }) {
-                game.canvas.width = Width;
-                game.canvas.height = Height;
-                const context = game.canvas.getContext("2d");
-                for (const car of game.cars) {
-                    context.drawImage(game.images[car.images[car.frame]], car.x, game.lanes[car.lane]);
-                }
+        ramp(Infinity, (_, { scope }) => {
+            scope.canvas.width = Width;
+            scope.canvas.height = Height;
+            const context = scope.canvas.getContext("2d");
+            for (const car of scope.cars) {
+                context.drawImage(scope.images[car.images[car.frame]], car.x, scope.lanes[car.lane]);
             }
         })
     ).
@@ -163,22 +161,15 @@ const fiber = Scheduler.run().
     // Animation loop: switch frame every at 10 FPS (every 100ms) and update
     // the progress bar for the duration of the game.
     spawn(fiber => fiber.
-        spawn(fiber => fiber.
-            repeat(fiber => fiber.
-                effect(({ value: game }) => {
-                    for (const car of game.cars) {
-                        car.frame = 1 - car.frame;
-                    }
-                }).
-                delay(100)
-            )
-        ).
-        spawn(fiber => fiber.
-            ramp(GameDuration, {
-                rampDidProgress(p, { value: game }) {
-                    game.progress.value = p * GameDuration;
+        repeat(fiber => fiber.
+            sync(({ scope: { cars } }) => {
+                for (const car of cars) {
+                    car.frame = 1 - car.frame;
                 }
-            })
-        ).
-        join()
+            }).
+            ramp(100)
+        )
+    ).
+    spawn(fiber => fiber.
+        ramp(GameDuration, (p, { scope: { progress } }) => { progress.value = p * GameDuration; })
     );
