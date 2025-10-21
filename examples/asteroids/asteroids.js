b9a2357456b9bd0d89f5bbe84b5be6a00cd36dd7
stcucufa <104885483+stcucufa@users.noreply.github.com>
2025-10-21 08:57:43 +0200

    4X06 Update Asteroids example (#173)

--- examples/asteroids/asteroids.js
+++ examples/asteroids/asteroids.js
@@ -1,98 +1,211 @@
-import Scheduler from "../../lib/scheduler.js";
-import { First } from "../../lib/fiber.js";
-import { K } from "../../lib/util.js";
-import Game from "./game.js";
+import { run, Fiber, First, FirstValue } from "../../lib/shell.js";
+import Game, { Text } from "./game.js";
 
 const UpdateDuration = 1000 / Game.UpdateFPS;
 
-// Handle a key down/up event for a given fiber, looking for a specific key.
-function handleKey(fiber, key, on, off) {
-    const eventShouldBeIgnored = event => event.key !== key;
-    fiber.repeat(fiber => {
-        fiber.event(window, "keydown", {
-            eventShouldBeIgnored,
-            eventWasHandled(event) { event.preventDefault(); }
-        });
-        if (on) {
-            fiber.effect(on);
-        }
-        fiber.event(window, "keyup", { eventShouldBeIgnored });
-        if (off) {
-            fiber.effect(off);
-        }
-    });
-}
+run().
 
-Scheduler.run().
-    exec(() => new Game(document.querySelector("canvas"))).
+    // Create a game object and add it to the scope of the main fiber; this is
+    // also the initial value of the fiber.
+    call(({ scope }) => {
+        scope.game = new Game(document.querySelector("canvas"));
+        return scope.game;
+    }).
 
-    // Draw loop
-    spawn(fiber => fiber.named("draw-loop").
-        ramp(Infinity, {
-            rampDidProgress(_, { value: game }) {
-                game.draw();
-            }
-        })
-    ).
+    // Draw loop: draw the game.
+    spawn(fiber => fiber.ramp(Infinity, ({ value: game }) => { game.draw(); })).
 
-    // Enemies (asteroids, TODO: UFO)
-    spawn(fiber => fiber.named("enemies").
-        exec(({ value: game }) => Array(4).fill().map(() => game.asteroid())).
-        map(fiber => fiber.
-            effect(({ value: asteroid }) => { console.info(asteroid); }).
-            repeat(fiber => fiber.
-                delay(UpdateDuration).
-                effect(({ value: asteroid }) => { asteroid.update(); })
+    // Keyboard handling (see actual handlers below). Because we use regular
+    // event listeners, this only needs to be setup once.
+    call(({ value: game }) => {
+        window.addEventListener("keydown", event => keydown(event, game));
+        window.addEventListener("keyup", event => keyup(event, game));
+    }).
+
+    // Update loop: update all game objects and gather the list of new objects
+    // resulting from the updates, setting a timeout to remove all those that
+    // have a duration (particles).
+    spawn(fiber => fiber.
+        repeat(fiber => fiber.
+            ramp(UpdateDuration).
+            call(({ value: game }) => {
+                const [enter] = game.update();
+                return [...enter].filter(object => object.durationMs >= 0);
+            }).
+            mapspawn(fiber => fiber.
+                ramp(({ value: { durationMs } }) => durationMs).
+                call(({ value: object }) => { object.game.removeObject(object); })
             )
         )
     ).
 
-    // Player loop
-    spawn(fiber => fiber.named("ship").
-        exec(({ value: game }) => game.ship()).
-        store("ship").
-        repeat(fiber => fiber.
+    // Game loop.
+    repeat(fiber => fiber.
 
-            // Keys: left and right to turn, up to accelerate.
-            spawn(fiber => fiber.named("key-left").
-                macro(handleKey, "ArrowLeft",
-                    ({ value: ship }) => { ship.angularVelocity = -ship.maxAngularVelocity; },
-                    ({ value: ship }) => { ship.angularVelocity = Math.max(0, ship.angularVelocity); }
-                )
-            ).
-            spawn(fiber => fiber.named("key-right").
-                macro(handleKey, "ArrowRight",
-                    ({ value: ship }) => { ship.angularVelocity = ship.maxAngularVelocity; },
-                    ({ value: ship }) => { ship.angularVelocity = Math.min(0, ship.angularVelocity); }
-                )
-            ).
-            spawn(fiber => fiber.named("key-up").
-                macro(handleKey, "ArrowUp",
-                    ({ value: ship }) => { ship.acceleration = ship.maxAcceleration; },
-                    ({ value: ship }) => { ship.acceleration = ship.game.Friction; }
-                )
+        // Title screen.
+        call(({ value: game }) => { game.reset(); }).
+        append(text("ASTEROIDS")).
+
+        // Enemies
+        // FIXME 500E Asteroids: UFO
+        spawn(fiber => fiber.
+            repeat(fiber => fiber.
+                call((fiber, scheduler) => {
+                    const asteroidFiber = new Fiber().
+                        event(({ value: asteroid }) => asteroid, "collided", {
+                            eventWasHandled({ detail: { results } }) {
+                                for (const asteroid of results) {
+                                    wait(asteroid);
+                                }
+                            }
+                        });
+                    function wait(asteroid) {
+                        const scheduledFiber = scheduler.attachFiber(fiber, asteroidFiber);
+                        scheduledFiber.value = asteroid;
+                    }
+                    const game = fiber.value;
+                    for (let i = game.level; i > 0; --i) {
+                        wait(game.asteroid());
+                    }
+                }).
+                join().
+
+                // Next level
+                call(({ value: game }) => { game.level += 1; }).
+                call(({ value: game, scope }) => {
+                    game.inputs.clear();
+                    scope.text = game.addObject(new Text(`LEVEL ${game.level}`));
+                }).
+                spawn(fiber => fiber.event(({ value: game }) => game, "anykey")).
+                spawn(fiber => fiber.ramp(1000)).
+                join().
+                call(({ value: game, scope }) => {
+                    game.removeObject(scope.text);
+                    delete scope.text;
+                })
+            )
+        ).
+
+        // Player loop: spawn a new ship and wait for it to be destroyed, then for
+        // the debris to clear up before spawning a new one, as long as the player
+        // has lives left.
+        spawn(fiber => fiber.
+            repeat(fiber => fiber.
+
+                // Create a ship and use it as value for the fiber; also save to scope.
+                call(({ scope, value: game }) => {
+                    scope.ship = game.ship();
+                    return scope.ship;
+                }).
+
+                // Listen to the ship being removed to end the loop with the spawn
+                // delay duration (the longest that a debris particle can last).
+                event(({ value: ship }) => ship, "collided").
+                ramp(({ value: ship }) => ship.debrisDur[1]).
+
+                // Remove a life.
+                call(({ scope: { game } }) => { game.removeObject(game.lives.pop()); }),
+
+                // End when no mores ships remain.
+                { repeatShouldEnd: (_, { value: { lives } }) => lives.length === 0 }
             ).
 
-            // Ship updates
-            spawn(fiber => fiber.
-                repeat(fiber => fiber.
-                    delay(UpdateDuration).
-                    exec(({ scope: { ship } }) => ship.update()).
-                    map(fiber => fiber.
-                        spawn(fiber => fiber.
-                            repeat(fiber => fiber.
-                                delay(UpdateDuration).
-                                effect(({ value: particle }) => particle.update())
-                            )
-                        ).
-                        spawn(fiber => fiber.delay(({ value: { durationMs } }) => durationMs)).
-                        join(First).
-                        effect(({ value: particle }) => { particle.game.removeObject(particle); })
-                    )
-                )
-            ).
+            // Game over
+            append(text("GAME OVER"))
+        ).
 
-            // FIXME end when the ship is destroyed
-            join(First)
-        )
-    );
+        join(First)
+);
+
+// Keydown and keyup event handlers; translate raw inputs to input states of
+// the game. T is for thrust (up arrow), L/R for left/right rotation (left and
+// right arrow; allow both arrows to be pressed at the same time by using the
+// direction of the last pressed arrow).
+
+function keydown(event, { inputs }) {
+    if (event.altKey || event.ctrlKey || event.isComposing || event.metaKey || event.shiftKey) {
+        return;
+    }
+    inputs.add(event.key);
+    switch (event.key) {
+        case "ArrowLeft":
+            if (!event.repeat) {
+                if (inputs.has("Right")) {
+                    inputs.delete("Right");
+                    inputs.add("RL");
+                }
+                inputs.add("Left");
+            }
+            break;
+        case "ArrowRight":
+            if (!event.repeat) {
+                if (inputs.has("Left")) {
+                    inputs.delete("Left");
+                    inputs.add("LR");
+                }
+                inputs.add("Right");
+            }
+            break;
+        case "ArrowUp":
+            if (!event.repeat) {
+                inputs.add("Thrust");
+            }
+            break;
+        case "z":
+            if (!event.repeat) {
+                inputs.add("Shoot");
+            }
+            break;
+        case "ArrowDown":
+        case " ":
+            // Do nothing but avoid scrolling the page
+            break;
+        default:
+            return;
+    }
+    event.preventDefault();
+}
+
+function keyup(event, game) {
+    const { inputs } = game;
+    const anykey = inputs.has(event.key);
+    switch (event.key) {
+        case "ArrowLeft":
+            inputs.delete("Left");
+            if (inputs.has("RL") || inputs.has("LR")) {
+                inputs.delete("RL");
+                inputs.delete("LR");
+                inputs.add("Right");
+            }
+            break;
+        case "ArrowRight":
+            inputs.delete("Right");
+            if (inputs.has("RL") || inputs.has("LR")) {
+                inputs.delete("RL");
+                inputs.delete("LR");
+                inputs.add("Left");
+            }
+            break;
+        case "ArrowUp":
+            inputs.delete("Thrust");
+            break;
+    }
+    inputs.delete(event.key);
+    if (anykey && !inputs.has(event.key)) {
+        game.customEvent("anykey");
+    }
+}
+
+// Show text and wait for any key before removing it.
+function text(t) {
+    return fiber => fiber.
+        call(({ value: game, scope }) => {
+            game.inputs.clear();
+            scope.text = game.addObject(new Text(t));
+        }).
+        event(({ value: game }) => game, "anykey").
+        call(({ value: game, scope }) => {
+            game.removeObject(scope.text);
+            delete scope.text;
+        });
+}
--- examples/asteroids/game.js
+++ examples/asteroids/game.js
@@ -1,26 +1,49 @@
-import { clamp, remove } from "../../lib/util.js";
+import { clamp, customEvent, random, remove } from "../../lib/util.js";
 
 const π = Math.PI;
 const τ = 2 * π;
 
-export default class Game {
-    Friction = -0.125;
-    StarsCount = 1111;
-    static UpdateFPS = 60;
+const ForegroundColor = "#f8f9f0";
+const HighlightColor = "#ffff40";
 
+// The game object manages the all the objects that are drawn, updated, and
+// colliding.
+export default class Game extends EventTarget {
+    StarsCount = 1111;
+
+    // Create a new game in the given canvas (used for drawing), starting
+    // with a background of stars.
     constructor(canvas) {
+        super();
         this.canvas = canvas;
+        this.objects =  [];
         this.width = canvas.clientWidth;
         this.height = canvas.clientHeight;
-        this.objects = [new Background(this)];
+        this.inputs = new Set();
     }
 
+    // Call customEvent on the game object directly.
+    customEvent(type, detail) {
+        customEvent.call(this, type, detail);
+    }
+
+    // Reset game: new background and reset all ships.
+    reset() {
+        this.objects = [new Background(this)];
+        this.collidesWithAsteroid = [];
+        this.collidesWithBullet = [];
+        this.lives = new Array(3).fill().map((_, i) => this.addObject(new Life(i)));
+        this.level = 1;
+    }
+
+    // Get a clear drawing context at the right device pixel ratio.
     get context() {
         this.canvas.width = this.width * window.devicePixelRatio;
         this.canvas.height = this.height * window.devicePixelRatio;
         return this.canvas.getContext("2d");
     }
 
+    // Draw all objects in the game, in that order (so background comes first).
     draw() {
         const context = this.context;
         context.save();
@@ -31,29 +54,76 @@ export default class Game {
         context.restore();
     }
 
+    // All values for acceleration, velocity, &c. are based on this update
+    // frequency.
+    static UpdateFPS = 60;
+
+    // Update all game objects by calling their update function, collecting
+    // the set of objects that are leaving (e.g., the ship or an asteroid
+    // exploding) or entering (e.g., particles or smaller asteroids).
+    update() {
+        const enter = new Set();
+        const leave = new Set();
+        for (const object of this.objects) {
+            object.update?.(enter, leave, this.inputs);
+        }
+        for (const object of leave) {
+            this.removeObject(object);
+        }
+        for (const object of enter) {
+            this.addObject(object);
+        }
+        // Return the sets of objects entering and leaving.
+        return [enter, leave];
+    }
+
+    // Add an object to the game, setting its `game` property back to this.
     addObject(object) {
         this.objects.push(object);
         object.game = this;
+        if (object.collidesWithAsteroid) {
+            this.collidesWithAsteroid.push(object);
+        }
+        if (object.collidesWithBullet) {
+            this.collidesWithBullet.push(object);
+        }
         return object;
     }
 
+    // Remove an object from the game, deleting its `game` property.
     removeObject(object) {
         remove(this.objects, object);
+        if (object.collidesWithAsteroid) {
+            remove(this.collidesWithAsteroid, object);
+        }
+        if (object.collidesWithBullet) {
+            remove(this.collidesWithBullet, object);
+        }
+        if (object === this.ship) {
+            delete this.ship;
+        }
+        delete object.game;
     }
 
+    // Add a new ship to the game at the center of the screen.
     ship() {
-        return this.addObject(new Ship(this.width / 2, this.height / 2, -π / 2));
+        this.inputs.clear();
+        return this.ship = this.addObject(new Ship(this.width / 2, this.height / 2, -π / 2));
     }
 
+    // Add a new asteroid to the game at a random position.
     asteroid() {
         return this.addObject(new Asteroid(this.width * Math.random(), this.height * Math.random()));
     }
 }
 
+// Background: a static star field. It is generated once per game and then
+// drawn as the background of every frame.
 class Background {
     BgColor = "#222";
-    FgColor = "#f8f9f0";
+    FgColor = ForegroundColor;
 
+    // Generate the image data to be drawn on every frame.
     constructor(game) {
         const context = game.context;
         const width = context.canvas.width;
@@ -74,13 +144,35 @@ class Background {
         this.imageData = context.getImageData(0, 0, width, height);
     }
 
+    // Draw the background.
     draw(context) {
         context.putImageData(this.imageData, 0, 0);
     }
 }
 
-class Sprite {
-    fgColor = "#f8f9f0";
+export class Text {
+    FgColor = HighlightColor;
+
+    constructor(text) {
+        this.text = text;
+    }
+
+    draw(context, game) {
+        context.save();
+        context.textAlign = "center";
+        context.textBaseline = "middle";
+        context.font = `96px system-ui`;
+        context.fillStyle = this.FgColor;
+        context.fillText(this.text, game.width / 2, game.height / 2);
+        context.restore();
+    }
+}
+
+// Base class for sprites (all moving objects in the game, including
+// particles). Sprites have position, radius, velocity, acceleration,
+// heading and angular velocity.
+class Sprite extends EventTarget {
+    fgColor = ForegroundColor;
     lineWidth = 2;
     lineJoin = "round";
     angularVelocity = 0;
@@ -89,9 +181,15 @@ class Sprite {
     maxVelocity = Infinity;
 
     constructor(x, y, angle = 0) {
+        super();
         this.x = x;
         this.y = y;
         this.angle = angle;
+    }
+
+    // Call customEvent on the sprite object directly.
+    customEvent(type, detail) {
+        customEvent.call(this, type, detail);
     }
 
     update() {
@@ -99,6 +197,16 @@ class Sprite {
         this.velocity = clamp(this.velocity + this.acceleration, 0, this.maxVelocity)
         this.x = (this.game.width + this.x + this.velocity * Math.cos(this.heading ?? this.angle)) % this.game.width;
         this.y = (this.game.height + this.y + this.velocity * Math.sin(this.heading ?? this.angle)) % this.game.height;
+    }
+
+    resolveCollisions(enter, leave, others) {
+        for (const other of others) {
+            if (collides(this, other)) {
+                leave.add(other);
+                const results = other.collided(enter, this);
+                other.customEvent("collided", { with: this, results });
+            }
+        }
     }
 
     beginPath(context) {
@@ -133,8 +241,27 @@ class PointParticle extends Sprite {
     }
 }
 
+class Bullet extends PointParticle {
+    fgColor = HighlightColor;
+    velocity = 10;
+    radius = 3;
+    durationMs = 700;
+    disappearsOnCollision = true;
+
+    constructor(x, y, angle) {
+        super(x, y);
+        this.angle = angle;
+    }
+
+    update(enter, leave) {
+        super.update(enter, leave);
+        this.resolveCollisions(enter, leave, this.game.collidesWithBullet);
+    }
+}
+
 class ExhaustParticle extends PointParticle {
-    fgColor = "#ffff40";
+    fgColor = HighlightColor;
+    dur = [100, 200];
 
     constructor(x, y, angle, velocity) {
         const d = 2 * (1 + Math.random());
@@ -143,19 +270,38 @@ class ExhaustParticle extends PointParticle {
         this.heading = h;
         this.velocity = velocity;
         this.radius = 2 * Math.random();
-        this.durationMs = 100 * (1 + Math.random());
+        this.durationMs = random(...this.dur);
+    }
+}
+
+class DebrisParticle extends Sprite {
+    constructor(x, y, radius, velocity, dur) {
+        super(x, y, Math.random() * τ);
+        this.radius = radius;
+        this.velocity = velocity;
+        this.angularVelocity = Math.random() * 0.1;
+        this.heading = Math.random() * τ;
+        this.durationMs = dur;
+    }
+
+    draw(context) {
+        this.beginPath(context);
+        context.moveTo(-this.radius, 0);
+        context.lineTo(this.radius, 0);
+        this.stroke(context);
     }
 }
 
 class Ship extends Sprite {
     radius = 8;
     maxVelocity = 8;
-    maxAngularVelocity = 0.1;
-    maxAcceleration = 1;
-
-    constructor(x, y, angle) {
-        super(x, y, angle);
-    }
+    maxAngularVelocity = 0.07;
+    maxAcceleration = 0.4;
+    friction = -0.035;
+    collidesWithAsteroid = true;
+    debris = [4, 8];
+    debrisVelocity = 0.1;
+    debrisDur = [1000, 2000];
 
     draw(context) {
         this.beginPath(context);
@@ -167,31 +313,91 @@ class Ship extends Sprite {
         this.stroke(context);
     }
 
-    update() {
-        super.update();
-        const particles = [];
+    get dx() {
+        return 2 * this.radius * Math.cos(this.angle);
+    }
+
+    get dy() {
+        return 2 * this.radius * Math.sin(this.angle);
+    }
+
+    // Set the acceleration and angular velocity based on inputs then emit
+    // exhaust particles if thrusting.
+    update(enter, leave, inputs) {
+        this.acceleration = inputs.has("Thrust") ? this.maxAcceleration : this.friction;
+        this.angularVelocity = inputs.has("Right") ? this.maxAngularVelocity :
+            inputs.has("Left") ? -this.maxAngularVelocity : 0;
+        super.update(enter, leave, inputs);
+        if (inputs.has("Shoot")) {
+            inputs.delete("Shoot");
+            enter.add(new Bullet(this.x + this.dx, this.y + this.dy, this.angle));
+        }
         if (this.acceleration > 0) {
             const n = 10 * Math.random();
             for (let i = 0; i < n; ++i) {
-                particles.push(this.game.addObject(new ExhaustParticle(this.x, this.y, this.angle, 0.1 * this.velocity)));
+                enter.add(new ExhaustParticle(this.x - this.dx, this.y - this.dy, this.angle, 0.1 * this.velocity));
             }
         }
-        return particles;
+    }
+
+    // Emit debris particle after a collision.
+    collided(enter) {
+        const velocity = this.maxVelocity * this.debrisVelocity;
+        return Array(random(...this.debris)).fill().map(() => {
+            const debris = new DebrisParticle(
+                this.x, this.y, this.radius * (0.5 + Math.random()), velocity, random(...this.debrisDur)
+            );
+            enter.add(debris);
+            return debris;
+        });
+    }
+}
+
+class Life extends Sprite {
+    constructor(i) {
+        const radius = 4;
+        super(4 * (i + 1) * radius, 4 * radius);
+        this.angle = -Math.PI / 2;
+        this.radius = radius;
+    }
+
+    draw(context) {
+        Ship.prototype.draw.call(this, context);
+    }
+}
+
+class DustParticle extends PointParticle {
+    dur = [200, 500];
+
+    constructor(x, y, velocity) {
+        super(x, y);
+        this.velocity = velocity;
+        this.heading = τ * Math.random();
+        this.radius = 2 * Math.random();
+        this.durationMs = random(...this.dur);
     }
 }
 
 class Asteroid extends Sprite {
-    minRadius = 12;
-    maxRadius = 32;
+    minRadius = 10;
+    maxRadius = 24;
     startVelocity = 2;
     maxAngularVelocity = 0.01;
+    collidesWithBullet = true;
+    debrisDur = [500, 1000];
 
-    constructor(x, y) {
+    constructor(x, y, parent, heading) {
         super(x, y);
-        this.radius = this.maxRadius;
-        this.heading = Math.random() * τ;
-        this.velocity = this.startVelocity;
-        this.angularVelocity = this.maxAngularVelocity * Math.random();
+        if (parent) {
+            this.radius = parent.radius / Math.sqrt(2);
+            this.velocity = parent.velocity * (Math.max(1, 0.5 + Math.random()));
+            this.angularVelocity = parent.angularVelocity * (1 + Math.random());
+        } else {
+            this.radius = this.maxRadius;
+            this.velocity = this.startVelocity;
+            this.angularVelocity = this.maxAngularVelocity * Math.random();
+        }
+        this.heading = heading ?? τ * Math.random();
         const n = (this.minRadius + this.maxRadius) / 2;
         this.points = Array(n).fill().map((_, i) => {
             const θ = τ * i / n;
@@ -210,4 +416,33 @@ class Asteroid extends Sprite {
         context.closePath();
         this.stroke(context);
     }
+
+    update(enter, leave) {
+        super.update(enter, leave);
+        this.resolveCollisions(enter, leave, this.game.collidesWithAsteroid);
+    }
+
+    // Split the asteroid in two (unless it is at the smallest size), making
+    // debris in the process.
+    collided(enter, other) {
+        for (let i = this.radius * (3 + Math.random()); i >= 0; --i) {
+            enter.add(new DustParticle(other.x, other.y, this.velocity * 0.5 * Math.random()));
+        }
+        other.disabled = true;
+        return this.radius > this.minRadius ? [π / 2, -π / 2].map(h => {
+            const asteroid = new Asteroid(this.x, this.y, this, this.heading + h);
+            enter.add(asteroid);
+            return asteroid;
+        }) : [];
+    }
+}
+
+// Test whether two game objects `a` and `b` collide based on the distance
+// between their centers and their respective radius.
+function collides(a, b) {
+    if (a.disabled || b.disabled) {
+        return false;
+    }
+    const distance = Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+    return distance < (a.radius + b.radius);
 }
